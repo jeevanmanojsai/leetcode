c914dad6254deabf349adbd4284c88fc60b851d9
Bara Jeevan Manoj Sai <jeevanmanojsai14@gmail.com>
2025-07-23 12:17:09 +0530

    Time: 0 ms (100%), Space: 16.2 MB (78.49%) - LeetHub

---
+++ 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int findSpecialInteger(vector<int>& arr) {
+        int n=arr.size();
+        int quater=n/4;
+
+        for(int i=0;i<n-quater ;i++){
+            if(arr[i]==arr[i+quater]){
+                return arr[i];
+            }
+        }
+        return -1;
+    }
+};
