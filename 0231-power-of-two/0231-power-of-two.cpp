9da7bc9ce7cff666367e1cedae1797a81061e5db
Bara Jeevan Manoj Sai <jeevanmanojsai14@gmail.com>
2025-07-29 11:31:08 +0530

    Time: 0 ms (100%), Space: 7.9 MB (18.03%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,10 +1,21 @@
+// class Solution {
+// public:
+//     bool isPowerOfTwo(int n) {
+//         for(int i=0;i<31;i++){
+//             int answer=pow(2,i);
+//             if(answer==n) return true;
+//         }
+//         return false;
+//     }
+// };
+
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        for(int i=0;i<31;i++){
-            int answer=pow(2,i);
-            if(answer==n) return true;
+        if(n<=0) return false;
+        while(n%2==0){
+            n/=2;
         }
-        return false;
+        return n==1;
     }
 };
