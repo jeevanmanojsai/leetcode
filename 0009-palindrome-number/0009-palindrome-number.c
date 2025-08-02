ed886bdfb32f380d9b75fb28230b6d9cc51fe6ca
Bara Jeevan Manoj Sai <jeevanmanojsai14@gmail.com>
2025-08-02 09:57:55 +0530

    Time: 0 ms (100%), Space: 8 MB (99.71%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.c
+++ 0009-palindrome-number/0009-palindrome-number.c
@@ -1,10 +1,9 @@
 bool isPalindrome(int x){
-    if(x<0 || x!=0 && x%10 ==0 )
-     return false;
+    if(x<0||x%10==0&&x!=0) return false;
     int check=0;
     while(x>check){
-        check = check*10 + x%10;
+        check=check*10+x%10;
         x/=10;
     }
-    return (x==check || x==check/10);
+    return (x==check||x==check/10);
 }
