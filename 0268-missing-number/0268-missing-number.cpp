4d153a93a78f0ab1d1b4b80450d76455a0c6a2c6
Bara Jeevan Manoj Sai <jeevanmanojsai14@gmail.com>
2025-09-06 14:03:57 +0530

    Time: 0 ms (100%), Space: 21.6 MB (92.41%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,10 +1,23 @@
+// class Solution {
+// public:
+//     int missingNumber(vector<int>& nums) {
+//         int ans=nums.size();
+//         for(int i=1;i<nums.size();i++){
+//             // ans+=i-nums[i];
+//             int res=ans+i-nums[i];
+//         }
+//         return ans;
+//     }
+// };
+
+
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int ans=nums.size();
-        for(int i=0;i<nums.size();i++){
-            ans+=i-nums[i];
-        }
-        return ans;
-    }
+      int n=nums.size();
+      sort(nums.begin(),nums.end()); 
+   for(int i=0;i<n;i++){
+    if(nums[i]!=i)
+    return i;}   
+     return n;}
 };
