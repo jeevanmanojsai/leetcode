3c1c63b1a130dd2bbcd29528978108a220683bd8
Bara Jeevan Manoj Sai <jeevanmanojsai14@gmail.com>
2025-09-12 15:16:51 +0530

    Time: 0 ms (100%), Space: 7.8 MB (53.16%) - LeetHub

--- 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.cpp
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.cpp
@@ -1,14 +1,29 @@
+// class Solution {
+// public:
+//     int totalMoney(int n) {
+//         int mon = 1;
+//         int ans = 0;
+//         while (n > 0) {
+//             for (int day = 0; day < min(n, 7); day++) {
+//                 ans += mon + day;
+//             }
+//             n -= 7;
+//             mon++;
+//         }
+//         return ans;
+//     }
+// };
+
+
+
+
 class Solution {
 public:
     int totalMoney(int n) {
-        int mon = 1;
-        int ans = 0;
-        while (n > 0) {
-            for (int day = 0; day < min(n, 7); day++) {
-                ans += mon + day;
-            }
-            n -= 7;
-            mon++;
+        int ans = 0, weekStart = 1;
+        for (int day = 0; day < n; day++) {
+            ans += weekStart + (day % 7);  
+            if (day % 7 == 6) weekStart++; 
         }
         return ans;
     }
